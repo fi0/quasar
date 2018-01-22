8fb333fc60d6fc08a576698aa68a97289797cf96
Sena Heydari <sheydari@dosomething.org>
2018-01-22 17:08:21 -0500

    Linterati.

--- quasar/rogue_queue_test.py
+++ quasar/rogue_queue_test.py
@@ -30,21 +30,21 @@ class TestQueue(QuasarQueue):
         for i in range(100):
             print(''.join(("Publishing generic message {} to "
                            "queue {}.")).format(i, config.TEST_QUEUE))
-            message_data = {"data": { "data": { "timestamp": time.time() },
-                            "meta": { "message_num": i } } }
+            message_data = {"data": {"data": {"timestamp": time.time() },
+                            "meta": {"message_num": i } } }
             self.pub_message(self.body_encode(message_data))
 
     def pub_test_rogue_messages(self):
         for i in range(100):
             print(''.join(("Publishing Rogue test message {} to "
                            "queue {}.")).format(i, config.TEST_QUEUE))
-            message_data = {"data": { "data": { "timestamp": time.time() },
-                            "meta": { "message_source": "rogue" } } }
+            message_data = {"data": {"data": {"timestamp": time.time() },
+                            "meta": {"message_source": "rogue" } } }
             self.pub_message(self.body_encode(message_data))
 
     def process_message(self, message_data):
         message = json.loads(message_data)
-        if ('message_source' in message['data']['meta'] and 
+        if ('message_source' in message['data']['meta'] and
                 message['data']['meta']['message_source'] == 'rogue'):
             print("Routing Rogue message to Rogue queue.")
             rogue_queue.pub_message(message_data)
@@ -60,11 +60,14 @@ test_queue = TestQueue()
 def pub_generic():
     test_queue.pub_generic_messages()
 
+
 def pub_rogue():
     test_queue.pub_test_rogue_messages()
 
+
 def test_consume():
     test_queue.start_consume()
+
 
 def rogue_consume():
     rogue_queue.start_consume()
