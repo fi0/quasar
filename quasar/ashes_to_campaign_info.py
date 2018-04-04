96c612c216265a11066ec731bf289b79f814cdec
Sena Heydari <sheydari@dosomething.org>
2018-04-04 14:28:47 -0400

    Updating to have both recreate and refresh options of materialized view for more efficiency.

--- quasar/ashes_to_campaign_info.py
+++ quasar/ashes_to_campaign_info.py
@@ -4,7 +4,7 @@ import time
 import sys
 
 
-def main():
+def create():
     start_time = time.time()
     """Keep track of start time of script."""
 
@@ -84,6 +84,22 @@ def main():
     end_time = time.time()  # Record when script stopped running.
     duration = end_time - start_time  # Total duration in seconds.
     print('duration: ', duration)
+
+def main():
+    start_time = time.time()
+    """Keep track of start time of script."""
+
+    db = Database()
+
+    db.query('REFRESH MATERIALIZED VIEW campaign_info')
+    db.disconnect()
+
+    end_time = time.time()  # Record when script stopped running.
+    duration = end_time - start_time  # Total duration in seconds.
+    print('duration: ', duration)
+
+if __name__ == "__create__":
+    create()
 
 if __name__ == "__main__":
     main()
