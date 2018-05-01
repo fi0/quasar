e2139f911376ecda48acce832753a6f616687910
Sena Heydari <sheydari@dosomething.org>
2018-05-01 18:43:53 -0400

    Update path_campaign_lookup to be materialized view instead of temp table.

--- quasar/misc/puck_etl.sql
+++ quasar/misc/puck_etl.sql
@@ -1,3 +1,4 @@
+REFRESH MATERIALIZED VIEW public.path_campaign_lookup;
 REFRESH MATERIALIZED VIEW public.phoenix_next_events;
 REFRESH MATERIALIZED VIEW public.phoenix_next_sessions;
 REFRESH MATERIALIZED VIEW public.device_northstar_crosswalk;
