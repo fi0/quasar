8b238ab2d39a1da8b1ad3f9ce742eace9a17cc5b
shasan <sohaib3@icloud.com>
2018-05-11 10:11:41 -0400

    add files to parse phoenix data from json blobs

---
+++ quasar/misc/puck_etl_json.sql
@@ -0,0 +1,4 @@
+REFRESH MATERIALIZED VIEW public.path_campaign_lookup;
+REFRESH MATERIALIZED VIEW public.phoenix_events;
+REFRESH MATERIALIZED VIEW public.phoenix_sessions;
+REFRESH MATERIALIZED VIEW public.device_northstar_crosswalk;
