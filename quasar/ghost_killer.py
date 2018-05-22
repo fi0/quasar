d1a748ee0e3514a97d9f9568d104e71626f6ed05
Sena Heydari <sheydari@dosomething.org>
2018-05-22 12:17:22 -0400

    Stickler Linter.

--- quasar/ghost_killer.py
+++ quasar/ghost_killer.py
@@ -3,15 +3,16 @@ import time
 
 db = Database()
 
+
 def remove_ghost_posts():
     # Get list of all posts to remove.
     posts = db.query(''.join(("SELECT DISTINCT p.id "
-                             "FROM rogue.signups s "
-                             "INNER JOIN (SELECT g.id "
-                             "FROM rogue.signups g WHERE g.why_participated = "
-                             "'why_participated_ghost_test') ghost "
-                             "ON s.id = ghost.id "
-                             "INNER JOIN rogue.posts p ON p.signup_id = s.id")))
+                              "FROM rogue.signups s "
+                              "INNER JOIN (SELECT g.id "
+                              "FROM rogue.signups g WHERE g.why_participated = "
+                              "'why_participated_ghost_test') ghost "
+                              "ON s.id = ghost.id "
+                              "INNER JOIN rogue.posts p ON p.signup_id = s.id")))
 
     # Copy all posts into ghost posts table to remove from official counts.
     for post in posts:
