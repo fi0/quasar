03b97c1b371d1e13b499783e53d8c558ba89d46c
Sena Heydari <sheydari@dosomething.org>
2018-06-06 15:03:13 -0400

    Updating refresh to only use mat view.

--- quasar/refresh_bertly.py
+++ quasar/refresh_bertly.py
@@ -1,14 +1,7 @@
-import os
-import time
-from .aws_dms import start_dms_refresh, refresh_finished
 from .database import Database
 
 
 def main():
-    bertly_dms = os.environ.get('BERTLY_DMS_ARN')
-    start_dms_refresh(bertly_dms)
-    time.sleep(10)
-    refresh_finished(bertly_dms)
     db = Database()
     print('Refreshing Bertly Clicks materialized view.')
     db.query('REFRESH MATERIALIZED VIEW public.bertly_clicks')
