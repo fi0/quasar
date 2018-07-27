204dd85b406b7d771e61a41ec42f197fecfb0836
Sena Heydari <sheydari@dosomething.org>
2018-07-27 10:53:28 -0400

    Recreate the Bertly.

---
+++ data/sql/derived-tables/bertly.sql
@@ -0,0 +1,21 @@
+DROP MATERIALIZED VIEW IF EXISTS public.bertly_clicks CASCADE;
+CREATE MATERIALIZED VIEW public.bertly_clicks AS (
+	SELECT 
+		*,
+		(regexp_split_to_array(
+			(regexp_split_to_array(
+				(regexp_split_to_array(
+					c.target_url, 'user')
+				)[2], E'[=:]+')
+			)[2], 
+			E'[^a-zA-Z0-9]')
+		)[1] AS northstar_id,
+		(regexp_split_to_array(b.target_url, 'broadcastid=', 'i'))[2] AS broadcast_id,
+		(CASE WHEN target_url ilike '%%source=web%%' THEN 'web' 
+			WHEN target_url ilike '%%source=email%%' THEN 'email'  
+			ELSE 'sms' 
+			END) AS SOURCE 
+	FROM bertly.clicks c
+);
+GRANT SELECT ON public.campaign_activity TO looker;
+GRANT SELECT ON public.campaign_activity TO dsanalyst;
