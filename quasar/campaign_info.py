2f400d4fa49bd2ba362d53987c8692bf1b54c736
Sena Heydari <sheydari@dosomething.org>
2018-07-30 11:36:37 -0400

    Inserting records into database table.

--- quasar/campaign_info.py
+++ quasar/campaign_info.py
@@ -11,6 +11,7 @@ scraper = DSOAuthScraper(os.getenv('PHOENIX_URI'))
 db = Database()
 
 def refresh_campaign_ids():
+    # Refresh list of campaign id's to scrape from Rogue signups.
     db.query("REFRESH MATERIALIZED VIEW phoenix.campaign_ids")
     logging.info('Phoenix campaigns have been refreshed from rogue.signups.')
 
@@ -18,6 +19,9 @@ def refresh_campaign_ids():
 def update_campaign_info():
     refresh_campaign_ids()
     ids = db.query('SELECT * FROM phoenix.campaign_ids')
+    # Remove all stale entries to ensure latest good data.
+    db.query("TRUNCATE phoenix.campaigns_json")    
+    # Query each campaign id currently active from Phoenix campaign api.
     try:
         for id in ids:
             path = os.getenv('PHOENIX_CAMPAIGN_API_PATH') + id[0]
@@ -25,6 +29,12 @@ def update_campaign_info():
             if result.status_code == 200:
                 info = result.json()
                 logging.info('Scraped campaign {}.'.format((id[0])))
+                # Insert raw json per campaign into db.
+                db.query_str(''.join(("INSERT INTO phoenix.campaigns_json "
+                                      "(campaign_data) VALUES (%s)")),
+                             (json.dumps(info),))
+                logging.info('Campaign {} added to db.'.format((id[0])))
+
             else:
                 logging.error(''.join(('{} status code returned for campaign '
                                       '{}.'.format(result.status_code, id[0]))))
