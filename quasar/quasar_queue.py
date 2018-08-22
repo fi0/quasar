c6fe07142a14a42e6f20d0c478b20d82a1de620f
Sena Heydari <sheydari@dosomething.org>
2018-08-22 01:47:41 -0400

    Moved try/catch further in function.

--- quasar/quasar_queue.py
+++ quasar/quasar_queue.py
@@ -188,28 +188,32 @@ class RogueQueue(QuasarQueue):
         self.db = Database()
 
     def _add_signup(self, signup_data):
-        self.db.query_str(''.join(("INSERT INTO rogue.signups "
-                                   "(id, northstar_id, campaign_id, "
-                                   "campaign_run_id, quantity, "
-                                   "why_participated, source, "
-                                   "source_details, details, "
-                                   "created_at, updated_at) "
-                                   "VALUES (%s,%s,%s,%s,%s,%s,"
-                                   "%s,%s,%s,%s,%s) ON CONFLICT "
-                                   "(id, updated_at) "
-                                   "DO NOTHING")),
-                          (signup_data['signup_id'],
-                           signup_data['northstar_id'],
-                           signup_data['campaign_id'],
-                           signup_data['campaign_run_id'],
-                           signup_data['quantity'],
-                           signup_data['why_participated'],
-                           signup_data['signup_source'],
-                           signup_data['source_details'],
-                           signup_data['details'],
-                           signup_data['created_at'],
-                           signup_data['updated_at']))
-        log("Signup {} ETL'd.".format(signup_data['signup_id']))
+        try:
+            self.db.query_str(''.join(("INSERT INTO rogue.signups "
+                                       "(id, northstar_id, campaign_id, "
+                                       "campaign_run_id, quantity, "
+                                       "why_participated, source, "
+                                       "source_details, details, "
+                                       "created_at, updated_at) "
+                                       "VALUES (%s,%s,%s,%s,%s,%s,"
+                                       "%s,%s,%s,%s,%s) ON CONFLICT "
+                                       "(id, updated_at) "
+                                       "DO NOTHING")),
+                              (signup_data['signup_id'],
+                               signup_data['northstar_id'],
+                               signup_data['campaign_id'],
+                               signup_data['campaign_run_id'],
+                               signup_data['quantity'],
+                               signup_data['why_participated'],
+                               signup_data['signup_source'],
+                               signup_data['source_details'],
+                               signup_data['details'],
+                               signup_data['created_at'],
+                               signup_data['updated_at']))
+            log("Signup {} ETL'd.".format(signup_data['signup_id']))
+        except:
+            logerr("Signup {} has an error, skipping.".format(signup_data['signup_id']))
+            pass
 
     def _delete_signup(self, signup_id, deleted_at):
         self.db.query_str(''.join(("INSERT INTO rogue.signups "
@@ -223,34 +227,38 @@ class RogueQueue(QuasarQueue):
         log("Signup {} deleted and archived.".format(signup_id))
 
     def _add_post(self, post_data):
-        self.db.query_str(''.join(("INSERT INTO rogue.posts "
-                                   "(id, signup_id, campaign_id, "
-                                   "northstar_id, "
-                                   "type, action, quantity, url, caption, "
-                                   "status, source, "
-                                   "source_details, signup_source, "
-                                   "remote_addr, created_at, "
-                                   "updated_at) VALUES "
-                                   "(%s,%s,%s,%s,%s,%s,%s,%s,"
-                                   "%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT "
-                                   "DO NOTHING")),
-                          (post_data['id'],
-                           post_data['signup_id'],
-                           post_data['campaign_id'],
-                           post_data['northstar_id'],
-                           post_data['type'],
-                           post_data['action'],
-                           post_data['quantity'],
-                           post_data['media']['url'],
-                           post_data['media']['caption'],
-                           post_data['status'],
-                           post_data['source'],
-                           post_data['source_details'],
-                           post_data['signup_source'],
-                           post_data['remote_addr'],
-                           post_data['created_at'],
-                           post_data['updated_at']))
-        log("Post {} ETL'd.".format(post_data['id']))
+        try:
+            self.db.query_str(''.join(("INSERT INTO rogue.posts "
+                                       "(id, signup_id, campaign_id, "
+                                       "northstar_id, "
+                                       "type, action, quantity, url, caption, "
+                                       "status, source, "
+                                       "source_details, signup_source, "
+                                       "remote_addr, created_at, "
+                                       "updated_at) VALUES "
+                                       "(%s,%s,%s,%s,%s,%s,%s,%s,"
+                                       "%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT "
+                                       "DO NOTHING")),
+                              (post_data['id'],
+                               post_data['signup_id'],
+                               post_data['campaign_id'],
+                               post_data['northstar_id'],
+                               post_data['type'],
+                               post_data['action'],
+                               post_data['quantity'],
+                               post_data['media']['url'],
+                               post_data['media']['caption'],
+                               post_data['status'],
+                               post_data['source'],
+                               post_data['source_details'],
+                               post_data['signup_source'],
+                               post_data['remote_addr'],
+                               post_data['created_at'],
+                               post_data['updated_at']))
+            log("Post {} ETL'd.".format(post_data['id']))
+        except:
+            logerr("Post {} has an error, skipping.".format(post_data['id']))
+            pass
 
     def _delete_post(self, post_id, deleted_at):
         # Set post status to 'deleted'.
@@ -357,23 +365,17 @@ class RogueQueue(QuasarQueue):
             if pydash.get(data, 'deleted_at'):
                 self._delete_signup(data['id'], data['deleted_at'])
             else:
-                try:
-                    self._add_signup(data)
-                except:
-                    pass
+                self._add_signup(data)
         elif data['meta']['type'] == 'post':
             if pydash.get(data, 'deleted_at'):
                 self._delete_post(data['id'], data['deleted_at'])
             else:
-                try:
-                    self._add_post(data)
-                    if (pydash.get(data, 'details') is None or
-                            pydash.get(data, 'details') == ''):
-                        pass
-                    else:
-                        self._add_post_details(data['id'], data['details'])
-                except:
+                self._add_post(data)
+                if (pydash.get(data, 'details') is None or
+                        pydash.get(data, 'details') == ''):
                     pass
+                else:
+                    self._add_post_details(data['id'], data['details'])
         else:
             logerr("Unknown rogue message type. Exiting.")
             sys.exit(1)
