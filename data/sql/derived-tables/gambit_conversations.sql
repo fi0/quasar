fd09693e1caa08d1c6473c6b954ec8d7e1137b5e
Sena Heydari <sheydari@dosomething.org>
2018-08-27 16:05:42 -0400

    Updated -> syntax, and corrected duplicate index issue.

--- data/sql/derived-tables/gambit_conversations.sql
+++ data/sql/derived-tables/gambit_conversations.sql
@@ -1,12 +1,17 @@
 DROP MATERIALIZED VIEW IF EXISTS gambit_conversations.conversations_flattened;
 CREATE MATERIALIZED VIEW gambit_conversations.conversations_flattened AS
 (SELECT
-   (records->>'campaignId')::varchar as campaign_id, to_timestamp((((records->'createdAt'->>'$date')::BIGINT) / 1000)::double precision) as created_at,
-   (records->'_id'->>'$oid')::varchar as conversation_id, (records->>'importSource')::varchar as import_source,
-   (records->'lastOutboundMessage'->>'$oid')::varchar as last_outbound_message, (records->>'paused')::varchar as paused,
-   (records->>'platform')::varchar as platform, (records->>'platformUserId')::varchar as platform_user_id,
-   (records->>'topic')::varchar as topic, to_timestamp((((records->'updatedAt'->>'$date')::BIGINT) / 1000)::double precision) as updated_at,
-   (records->>'userId')::varchar as user_id from gambit_conversations.conversations_json);
+   (records->'campaignId')::varchar as campaign_id,
+   to_timestamp((((records->'createdAt'->>'$date')::BIGINT) / 1000)::double precision) as created_at,
+   (records->'_id'->>'$oid')::varchar as conversation_id,
+   (records->'importSource')::varchar as import_source,
+   (records->'lastOutboundMessage'->>'$oid')::varchar as last_outbound_message,
+   (records->'paused')::varchar as paused,
+   (records->'platform')::varchar as platform,
+   (records->'platformUserId')::varchar as platform_user_id,
+   (records->'topic')::varchar as topic,
+   to_timestamp((((records->'updatedAt'->>'$date')::BIGINT) / 1000)::double precision) as updated_at,
+   (records->'userId')::varchar as user_id from gambit_conversations.conversations_json);
 
 CREATE INDEX conversationidi on gambit_conversations.conversations_flattened(conversation_id);
 CREATE INDEX platformuidi on gambit_conversations.conversations_flattened(platform_user_id);
--- data/sql/derived-tables/gambit_messages.sql
+++ data/sql/derived-tables/gambit_messages.sql
@@ -1,22 +1,27 @@
 DROP MATERIALIZED VIEW IF EXISTS gambit_conversations.messages_flattened CASCADE;
 CREATE MATERIALIZED VIEW gambit_conversations.messages_flattened AS
 (SELECT
-    (records->>'agentId')::VARCHAR as agent_id,
+    (records->'agentId')::VARCHAR as agent_id,
     ((records->'attachments')::JSONB->0->>'url')::VARCHAR as attachment_url,
     ((records->'attachments')::JSONB->0->>'contentType')::VARCHAR as attachment_content_type,
-    (records->>'broadcastId')::VARCHAR as broadcast_id, (records->>'campaignId')::VARCHAR as campaign_id,
+    (records->'broadcastId')::VARCHAR as broadcast_id,
+    (records->'campaignId')::VARCHAR as campaign_id,
     (records->'conversationId'->>'$oid')::VARCHAR as conversation_id,
     to_timestamp((((records->'createdAt'->>'$date')::BIGINT) / 1000)::DOUBLE PRECISION) as created_at,
-    (records->>'direction')::VARCHAR as direction, (records->>'_id')::VARCHAR as message_id,
-    (records->>'macro')::VARCHAR as macro, (records->>'match')::VARCHAR as match,
+    (records->'direction')::VARCHAR as direction,
+    (records->'_id')::VARCHAR as message_id,
+    (records->'macro')::VARCHAR as macro,
+    (records->'match')::VARCHAR as match,
     to_timestamp((((records->'metadata'->'delivery'->'deliveredAt'->>'$date')::BIGINT) / 1000)::DOUBLE PRECISION) as delivered_at,
     (records->'metadata'->'delivery'->>'totalSegments')::INT as total_segments,
-    (records->>'platformMessageId')::VARCHAR as platform_message_id, (records->>'template')::VARCHAR as template,
-    records->>'text' as text, (records->>'topic')::VARCHAR as topic, (records->>'userId')::VARCHAR as user_id
+    (records->'platformMessageId')::VARCHAR as platform_message_id,
+    (records->'template')::VARCHAR as template,
+    records->'text' as text, (records->'topic')::VARCHAR as topic,
+    (records->'userId')::VARCHAR as user_id
 FROM gambit_conversations.messages_json);
 
 CREATE INDEX platformmsgi on gambit_conversations.messages_flattened(platform_message_id);
-CREATE INDEX useridi on gambit_conversations.messages_flattened(user_id);
+CREATE INDEX usermidi on gambit_conversations.messages_flattened(user_id);
 
 GRANT SELECT on gambit_conversations.messages_flattened TO looker;
 GRANT SELECT on gambit_conversations.messages_flattened to dsanalyst;
