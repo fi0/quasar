4627c868c495ea78cbb3f7e3db0f37d188299422
shasan <sohaib3@icloud.com>
2018-08-31 18:25:25 -0400

    fucking commas

--- data/sql/derived-tables/gambit_messages.sql
+++ data/sql/derived-tables/gambit_messages.sql
@@ -40,21 +40,21 @@ UNION ALL
 (SELECT 
 	g.agent_id,
 	g.attachment_url,
-	g.attachment_content_type
-	g.broadcast_id
-	g.campaign_id
-	g.conversation_id
-	g.created_at
-	g.direction
-	g.message_id
-	g.macro
-	g."match"
+	g.attachment_content_type,
+	g.broadcast_id,
+	g.campaign_id,
+	g.conversation_id,
+	g.created_at,
+	g.direction,
+	g.message_id,
+	g.macro,
+	g."match",
 	g.delivered_at,
-	g.total_segments
-	g.platform_message_id
-	g.template
-	g.text
-	g.topic
+	g.total_segments,
+	g.platform_message_id,
+	g.template,
+	g.text,
+	g.topic,
 	u.northstar_id AS user_id
 FROM 
 	gambit_conversations.messages_flattened g 
