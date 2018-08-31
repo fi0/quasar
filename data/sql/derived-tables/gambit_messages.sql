337e4bf0156b7a61f313fad92532c9c742d93f6d
shasan <sohaib3@icloud.com>
2018-08-31 16:19:02 -0400

    json parsing tweaks

--- data/sql/derived-tables/gambit_messages.sql
+++ data/sql/derived-tables/gambit_messages.sql
@@ -1,27 +1,28 @@
 DROP MATERIALIZED VIEW IF EXISTS gambit_conversations.messages_flattened CASCADE;
 CREATE MATERIALIZED VIEW gambit_conversations.messages_flattened AS
 (SELECT
-    (records->'agentId')::VARCHAR as agent_id,
-    ((records->'attachments')::JSONB->0->>'url')::VARCHAR as attachment_url,
-    ((records->'attachments')::JSONB->0->>'contentType')::VARCHAR as attachment_content_type,
-    (records->'broadcastId')::VARCHAR as broadcast_id,
-    (records->'campaignId')::VARCHAR as campaign_id,
-    (records->'conversationId'->>'$oid')::VARCHAR as conversation_id,
-    to_timestamp((((records->'createdAt'->>'$date')::BIGINT) / 1000)::DOUBLE PRECISION) as created_at,
-    (records->'direction')::VARCHAR as direction,
-    (records->'_id')::VARCHAR as message_id,
-    (records->'macro')::VARCHAR as macro,
-    (records->'match')::VARCHAR as match,
-    to_timestamp((((records->'metadata'->'delivery'->'deliveredAt'->>'$date')::BIGINT) / 1000)::DOUBLE PRECISION) as delivered_at,
-    (records->'metadata'->'delivery'->>'totalSegments')::INT as total_segments,
-    (records->'platformMessageId')::VARCHAR as platform_message_id,
-    (records->'template')::VARCHAR as template,
-    records->'text' as text, (records->'topic')::VARCHAR as topic,
-    (records->'userId')::VARCHAR as user_id
+    records ->> 'agentId' AS agent_id,
+    records #>> '{attachments,url}' AS attachment_url,
+    records ->> '{attachments,contentType}'as attachment_content_type,
+    records ->> 'broadcastId' AS broadcast_id,
+    records ->> 'campaignId' AS campaign_id,
+    records #>> '{conversationId,$oid}' AS conversation_id,
+    to_timestamp((((records #>> '{createdAt,$date}')::BIGINT) / 1000)::DOUBLE PRECISION) AS created_at,
+    records ->> 'direction' AS direction,
+    records #>> '{_id,$oid}' AS message_id,
+    records ->> 'macro' AS macro,
+    records ->> 'match' AS match,
+    to_timestamp((records #> '{metadata,delivery}' #>> '{deliveredAt,$date}')::BIGINT / 1000::DOUBLE PRECISION) AS delivered_at,
+    (records #> '{metadata,delivery}' ->> 'totalSegments')::INT AS total_segments,
+    records ->> 'platformMessageId' AS platform_message_id,
+    records ->> 'template' AS template,
+    records ->> 'text' AS text, 
+    records ->> 'topic' AS topic,
+    records ->> 'userId' AS user_id
 FROM gambit_conversations.messages_json);
 
-CREATE INDEX platformmsgi on gambit_conversations.messages_flattened(platform_message_id);
-CREATE INDEX usermidi on gambit_conversations.messages_flattened(user_id);
+CREATE INDEX platformmsgi ON gambit_conversations.messages_flattened(platform_message_id);
+CREATE INDEX usermidi ON gambit_conversations.messages_flattened(user_id);
 
-GRANT SELECT on gambit_conversations.messages_flattened TO looker;
-GRANT SELECT on gambit_conversations.messages_flattened to dsanalyst;
+GRANT SELECT ON gambit_conversations.messages_flattened TO looker;
+GRANT SELECT ON gambit_conversations.messages_flattened to dsanalyst;
