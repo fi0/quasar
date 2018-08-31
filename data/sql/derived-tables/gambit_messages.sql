c45a650b41cb7166ff04098431691cec7ce2e874
shasan <sohaib3@icloud.com>
2018-08-31 18:16:27 -0400

    derived inbound messages view

--- data/sql/derived-tables/gambit_messages.sql
+++ data/sql/derived-tables/gambit_messages.sql
@@ -26,3 +26,47 @@ CREATE INDEX usermidi ON gambit_conversations.messages_flattened(user_id);
 
 GRANT SELECT ON gambit_conversations.messages_flattened TO looker;
 GRANT SELECT ON gambit_conversations.messages_flattened to dsanalyst;
+
+CREATE MATERIALIZED VIEW public.gambit_messages_inbound AS 
+(
+SELECT 
+	* 
+FROM 
+	gambit_conversations.messages_flattened f
+WHERE 
+	f.direction = 'inbound' 
+	AND f.user_is IS NOT NULL 
+UNION ALL 
+(SELECT 
+	g.agent_id,
+	g.attachment_url,
+	g.attachment_content_type
+	g.broadcast_id
+	g.campaign_id
+	g.conversation_id
+	g.created_at
+	g.direction
+	g.message_id
+	g.macro
+	g."match"
+	g.delivered_at,
+	g.total_segments
+	g.platform_message_id
+	g.template
+	g.text
+	g.topic
+	u.northstar_id AS user_id
+FROM 
+	gambit_conversations.messages_flattened g 
+LEFT JOIN 
+	gambit_conversations.conversations_flattened c 
+	ON g.conversation_id = c.conversation_id
+LEFT JOIN 
+	public.users u 
+	ON substring(c.platform_user_id, 3, 9) = u.mobile
+WHERE 
+	g.direction = 'inbound'
+	g.user_id IS NULL 
+	AND u.mobile IS NOT NULL
+	AND u.mobile <> '')
+)
