0f355403820a0e18e4980254677cb6771307e4b7
blisteringherb <morgan@solosuenos.com>
2018-09-13 16:34:14 -0400

    Fixing syntax

--- quasar/gambit_import.py
+++ quasar/gambit_import.py
@@ -52,7 +52,7 @@ def main():
 
     end_time = time.time()  # Record when script stopped running.
     duration = end_time - start_time  # Total duration in seconds.
-    log('duration: ' + duration)
+    log("duration: {}".format(duration))
 
 
 if __name__ == "__main__":
