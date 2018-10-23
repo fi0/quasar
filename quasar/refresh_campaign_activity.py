692be274a99b7f6921536643ef30ecaa1fc16e11
Sena Heydari <sheydari@dosomething.org>
2018-10-23 16:05:40 -0400

    Updating with unique indexes on campaign_activity for concurrent mat view refresh.

--- quasar/refresh_campaign_activity.py
+++ quasar/refresh_campaign_activity.py
@@ -7,11 +7,11 @@ def main():
     start_time = time.time()
     """Keep track of start time of script."""
 
-    db.query("REFRESH MATERIALIZED VIEW public.signups")
-    db.query("REFRESH MATERIALIZED VIEW public.latest_post")
-    db.query("REFRESH MATERIALIZED VIEW public.posts")
-    db.query("REFRESH MATERIALIZED VIEW public.reported_back")
-    db.query("REFRESH MATERIALIZED VIEW public.campaign_activity")
+    db.query("REFRESH MATERIALIZED VIEW CONCURRENTLY public.signups")
+    db.query("REFRESH MATERIALIZED VIEW CONCURRENTLY public.latest_post")
+    db.query("REFRESH MATERIALIZED VIEW CONCURRENTLY public.posts")
+    db.query("REFRESH MATERIALIZED VIEW CONCURRENTLY public.reported_back")
+    db.query("REFRESH MATERIALIZED VIEW CONCURRENTLY public.campaign_activity")
     db.disconnect()
 
     end_time = time.time()  # Record when script stopped running.
