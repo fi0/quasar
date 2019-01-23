389d85cc4e95948129488b4da1f3c5e33d4e9181
Sena Heydari <sheydari@dosomething.org>
2019-01-23 16:58:47 -0500

    Use multi-threaded pool executor to process backfill records.

--- quasar/cio_sent_backfill.py
+++ quasar/cio_sent_backfill.py
@@ -1,8 +1,10 @@
+from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 import os
 import requests
 from requests.auth import HTTPBasicAuth
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
+from sqlalchemy.sql import select
 from sqlalchemy.sql import text
 
 from .utils import log, logerr
@@ -21,14 +23,14 @@ pg_ssl = os.getenv('PG_SSL')
 # Setup SQL Alchemy postgres connection.
 engine = create_engine(URL(**pg_vars),
                        connect_args={'sslmode': pg_ssl})
-connection = engine.connect()
+conn = engine.connect()
 
 # Grab a page from C.io messages API with optional next param for pagination.
 def get_page(next_page=None):
     params = {'metric': os.getenv('CIO_API_METRIC'),
               'type': os.getenv('CIO_API_TYPE'),
               'limit': os.getenv('CIO_API_LIMIT'),
-              'next': next_page}
+              'start': next_page}
     user = os.getenv('CIO_API_USER')
     password = os.getenv('CIO_API_PASSWORD')
     uri = os.getenv('CIO_API_URI')
@@ -36,7 +38,7 @@ def get_page(next_page=None):
     return r.json()
 
 
-# Insert record atomically. 
+# Insert C.io email_sent record atomically. 
 def insert_record(message):
 	query = text(''.join(("INSERT INTO cio.email_sent(email_id, customer_id, "
 		                  "email_address, template_id, subject, timestamp)"
@@ -52,12 +54,36 @@ def insert_record(message):
 	    'subject': message['subject'],
 	    'timestamp': message['metrics']['sent']
 	}
-	connection.execute(query, **record)
+	conn.execute(query, **record)
+	log('Message ID {} processed.'.format(message['id']))
+
+
+# Get next page location.
+def get_bookmark():
+	s = "SELECT * FROM cio.sent_backfill_track"
+	result = conn.execute(s)
+	return result.fetchall()
+
+
+# Keep track of next page location.
+def insert_bookmark(next_page):
+	query = text(''.join(("INSERT INTO cio.sent_backfill_track(next_page) "
+		                  "VALUES (:next_page)")))
+	conn.execute("TRUNCATE cio.sent_backfill_track")
+	conn.execute(query, next_page=next_page)
 
 
 def main():
-	page = get_page()
-	for id in page['messages']:
-		insert_record(id)
-		log('Message ID is {}.'.format(id['id']))
-	log('The next page ID is {}.'.format(page['next']))
+	# Check if this is start of the run. If not, resume from last page.
+	if get_bookmark() is None:
+		page = get_page()
+	else:
+		page = get_page(next_page=get_bookmark())
+		insert_bookmark(page['next'])
+	# While there is a page of results, continue processing.
+	while page:
+		with PoolExecutor(max_workers=os.getenv('POOL_SIZE')) as executor:
+			for _ in executor.map(insert_record, page['messages']):
+				pass
+		page = get_page(next_page=get_bookmark())
+		insert_bookmark(page['next'])
