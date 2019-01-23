21be85e8ea1db3b95057d4696b4dc2811b9da0cb
Sena Heydari <sheydari@dosomething.org>
2019-01-23 14:36:22 -0500

    Adding DB connection with SQL Alchemy.

--- quasar/cio_sent_backfill.py
+++ quasar/cio_sent_backfill.py
@@ -1,9 +1,26 @@
 import os
 import requests
 from requests.auth import HTTPBasicAuth
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
 
 from .utils import log, logerr
 
+pg_vars = {
+    'drivername': os.getenv('PG_DRIVER'),
+    'username': os.getenv('PG_USER'),
+    'password': os.getenv('PG_PASSWORD'),
+    'host': os.getenv('PG_HOST'),
+    'port': os.getenv('PG_PORT'),
+    'database': os.getenv('PG_DATABASE')
+}
+
+pg_ssl = os.getenv('PG_SSL')
+
+engine = create_engine(URL(**pg_vars),
+                       connect_args={'sslmode': pg_ssl})
+
+connection = engine.connect()
 
 # Grab a page from C.io messages API with optional next param for pagination.
 def get_page(next_page=None):
