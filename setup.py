1a06de2534f35d7cdfc31c0b1ded78066b139425
Morgan Rich <mrich@dosomething.org>
2019-03-01 11:59:13 -0500

    Removing deprecated files for Gambit pipeline
    * Updated the setup.py to point to new commands and removed unnecessary
    ones
    * Gambit conversations flattened is no longer necessary
    * Updated etl monitoring to point to Fivetran tables

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="quasar",
-    version="2019.2.19.0",
+    version="2019.3.1.0",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
@@ -20,16 +20,8 @@ setup(
             'cio_bounced_backfill = quasar.cio_bounced_backfill:main',
             'cio_sent_backfill = quasar.cio_sent_backfill:main',
             'etl_monitoring = quasar.etl_monitoring:run_monitoring',
-            'gambit_create_raw = quasar.gambit:create_gambit_raw_json',
-            'gambit_create_full = quasar.gambit:create_gambit_full',
-            'gambit_conversations_create = quasar.gambit:create_gambit_conversations',
-            'gambit_conversations_refresh = quasar.gambit:refresh_gambit_conversations',
-            'gambit_import = quasar.gambit_import:main',
             'gambit_messages_create = quasar.gambit:create_gambit_messages',
             'gambit_messages_refresh = quasar.gambit:refresh_gambit_messages',
-            'gambit_refresh_full = quasar.gambit:refresh_gambit_full',
-            'ft_gambit_messages_create = quasar.gambit_ft:create_gambit_messages',
-            'ft_gambit_messages_refresh = quasar.gambit_ft:refresh_gambit_messages',
             'gtm_retention_create = quasar.create_gtm_retention:main',
             'gtm_retention_refresh = quasar.refresh_gtm_retention:main',
             'mam_retention_create = quasar.create_mam_retention:main',
