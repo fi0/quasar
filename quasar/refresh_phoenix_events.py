32f31033ccc4c02424f3111c347cdd918770b5fa
Sena Heydari <sheydari@dosomething.org>
2019-03-04 14:16:57 -0500

    Updating ft_ to ft_puck_ since we can only have one unique ft_ per schema.

--- quasar/refresh_phoenix_events.py
+++ quasar/refresh_phoenix_events.py
@@ -8,7 +8,7 @@ def main():
     """Keep track of start time of script."""
 
     db.query("REFRESH MATERIALIZED VIEW public.path_campaign_lookup")
-    db.query("REFRESH MATERIALIZED VIEW ft_heroku_wzsf6b3z.phoenix_utms")
+    db.query("REFRESH MATERIALIZED VIEW ft_puck_heroku_wzsf6b3z.phoenix_utms")
     db.query("REFRESH MATERIALIZED VIEW public.phoenix_events")
     db.query("REFRESH MATERIALIZED VIEW public.phoenix_sessions")
     db.query("REFRESH MATERIALIZED VIEW public.device_northstar_crosswalk")
