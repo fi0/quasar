a6cf096bc048c219a8bfa9302eea1dcdf42efb83
shasan <sohaib3@icloud.com>
2019-04-10 15:18:14 -0400

    variabalize all the things like I think im supposed to

--- data/sql/derived-tables/post_actions.sql
+++ data/sql/derived-tables/post_actions.sql
@@ -1,10 +1,10 @@
-DROP MATERIALIZED VIEW IF EXISTS public.post_actions CASCADE;
-CREATE MATERIALIZED VIEW public.post_actions AS (
+DROP MATERIALIZED VIEW IF EXISTS :ft_rogue_actions CASCADE;
+CREATE MATERIALIZED VIEW :ft_rogue_actions AS (
 	SELECT 
 		*
 	FROM :ft_rogue_actions
 )
 ;
-CREATE UNIQUE INDEX ON public.post_actions (created_at, id);
-GRANT SELECT ON public.post_actions TO looker;
-GRANT SELECT ON public.post_actions TO dsanalyst;
+CREATE UNIQUE INDEX ON :ft_rogue_actions (created_at, id);
+GRANT SELECT ON :ft_rogue_actions TO looker;
+GRANT SELECT ON :ft_rogue_actions TO dsanalyst;
