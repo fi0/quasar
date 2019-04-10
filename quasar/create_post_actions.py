d4675d6b1a2e33eb9ddbd9034ab175f35e71e8e7
Sena Heydari <sheydari@dosomething.org>
2019-04-10 15:16:46 -0400

    Linting.

--- quasar/create_post_actions.py
+++ quasar/create_post_actions.py
@@ -4,6 +4,7 @@ from .sql_utils import run_sql_file
 
 data = {'ft_rogue_actions': os.getenv('FT_ROGUE_ACTIONS')}
 
+
 def main():
     run_sql_file('./data/sql/derived-tables/post_actions.sql', data)
 
