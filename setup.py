560623e30691925719d72df277f8d349460d8b6a
Morgan Rich <mrich@dosomething.org>
2019-06-05 10:11:33 -0400

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name="quasar",
-    version="2019.5.30.0",
+    version="2019.6.5.0",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
