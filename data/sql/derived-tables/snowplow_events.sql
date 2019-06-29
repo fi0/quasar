55bb0260aa4dca04cdadd0c2eb98d724b74b77ae
Sena Heydari <sheyd@users.noreply.github.com>
2019-06-28 17:28:34 -0700

    Adding first draft of snowplow based phoenix_events table. (#784)
    * Adding first draft of snowplow based phoenix_events table.
    
    * defining page views in event_name and updating indexes
    
    * join to public.campaign_info to grab `campaign_name`
    
    * remove `href` per convo with Sena that it was inconsistently used
    
    and intended to be deprecated
    
    * rename table and cast e.campaign_id to int
    
    * bug fix to point to the correct field to find page views
    
    * draft sessions table
    
    * Adding refresh for Snowplow phoenix events and sessions table.

--- data/sql/derived-tables/snowplow_events.sql
+++ data/sql/derived-tables/snowplow_events.sql
@@ -24,6 +24,7 @@ CREATE TABLE public.snowplow_base_event AS
 CREATE INDEX base_event_id ON public.snowplow_base_event (event_id);
 GRANT SELECT ON public.snowplow_base_event TO dsanalyst;
 
+
 DROP TABLE IF EXISTS public.snowplow_payload_event;
 CREATE TABLE public.snowplow_payload_event AS
 (SELECT
@@ -38,6 +39,7 @@ CREATE TABLE public.snowplow_payload_event AS
 );
 CREATE INDEX payload_event_id ON public.snowplow_payload_event (event_id);
 GRANT SELECT ON public.snowplow_payload_event TO dsanalyst;
+
 
 DROP TABLE IF EXISTS public.snowplow_raw_events;
 CREATE TABLE public.snowplow_raw_events AS
@@ -72,3 +74,87 @@ CREATE TABLE public.snowplow_raw_events AS
 );
 CREATE INDEX raw_event_id ON public.snowplow_raw_events (event_id);
 GRANT SELECT ON public.snowplow_raw_events TO dsanalyst;
+
+
+DROP TABLE IF EXISTS public.snowplow_phoenix_events;
+CREATE TABLE public.snowplow_phoenix_events AS (
+    SELECT
+        e.event_id,
+        e.event_datetime,
+        CASE WHEN e.event_name IS NULL
+	    AND e.event_type = 'pv'
+	    THEN 'view' ELSE e.event_name END AS event_name,
+        e.event_source,
+        e."path",
+        e."host",
+        e.utm_source AS page_utm_source,
+        e.utm_medium AS page_utm_medium,
+        e.utm_campaign AS page_utm_campaign,
+        e.campaign_id,
+	    i.campaign_name,
+        e.modal_type,
+        e.session_id,
+        e.browser_size,
+        e.northstar_id,
+        e.device_id
+    FROM public.snowplow_raw_events e
+    LEFT JOIN public.campaign_info i ON i.campaign_id = e.campaign_id::bigint
+);
+CREATE UNIQUE INDEX spe_unique ON public.snowplow_phoenix_events (event_datetime, event_name, event_id);
+CREATE INDEX spe_session_id ON public.snowplow_phoenix_events (session_id);
+GRANT SELECT ON public.snowplow_phoenix_events TO looker;
+GRANT SELECT ON public.snowplow_phoenix_events TO dsanalyst;
+
+
+DROP TABLE IF EXISTS public.snowplow_sessions;
+CREATE TABLE public.snowplow_sessions AS (
+    WITH sessions AS (
+	SELECT
+	    session_id,
+	    min(device_id) AS device_id,
+	    min(event_datetime) AS landing_datetime,
+	    max(event_datetime) AS ending_datetime,
+	    date_part(
+		'seconds', max(event_datetime) - min(event_datetime)
+	    ) AS session_duration_seconds,
+	    count(DISTINCT CASE WHEN event_name = 'view' THEN "path" END) AS num_pages_viewed
+	FROM snowplow_phoenix_events
+	GROUP BY session_id
+    ),
+    entry_exit_pages AS (
+	SELECT DISTINCT
+	    session_id,
+	    first_value("path") OVER (PARTITION BY session_id ORDER BY event_datetime 
+		ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS landing_page,
+	    last_value("path") OVER (PARTITION BY session_id ORDER BY event_datetime 
+		ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS exit_page
+	FROM snowplow_phoenix_events
+    ),
+    time_between_sessions AS (
+	SELECT DISTINCT
+	    device_id,
+	    session_id,
+	    LAG(ending_datetime) OVER (PARTITION BY device_id ORDER BY landing_datetime
+		ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+	    ) AS prev_session_endtime
+	FROM sessions
+    )
+    SELECT
+	s.session_id,
+	s.device_id,
+	s.landing_datetime,
+	s.ending_datetime,
+	s.session_duration_seconds,
+	s.num_pages_viewed,
+	p.landing_page,
+	p.exit_page,
+	date_part('day', s.landing_datetime - t.prev_session_endtime) AS days_since_last_session
+    FROM sessions s
+    LEFT JOIN entry_exit_pages p
+    ON p.session_id = s.session_id
+    LEFT JOIN time_between_sessions t
+    ON t.session_id = s.session_id
+);
+CREATE INDEX sps_landing ON public.snowplow_sessions (landing_datetime, landing_page);
+GRANT SELECT ON public.snowplow_sessions TO looker;
+GRANT SELECT ON public.snowplow_sessions TO dsanalyst;
--- data/sql/derived-tables/snowplow_events_refresh.sql
+++ data/sql/derived-tables/snowplow_events_refresh.sql
@@ -91,3 +91,94 @@ INSERT INTO public.snowplow_raw_events
   SELECT * FROM public.snowplow_raw_events_stage;
 CREATE INDEX event_index ON public.snowplow_raw_events (event_id);
 DROP TABLE IF EXISTS public.snowplow_raw_events_stage;
+
+DROP TABLE IF EXISTS public.snowplow_phoenix_events_stage;
+CREATE TABLE public.snowplow_phoenix_events_stage AS (
+    SELECT
+        e.event_id,
+        e.event_datetime,
+        CASE WHEN e.event_name IS NULL
+        AND e.event_type = 'pv'
+        THEN 'view' ELSE e.event_name END AS event_name,
+        e.event_source,
+        e."path",
+        e."host",
+        e.utm_source AS page_utm_source,
+        e.utm_medium AS page_utm_medium,
+        e.utm_campaign AS page_utm_campaign,
+        e.campaign_id,
+        i.campaign_name,
+        e.modal_type,
+        e.session_id,
+        e.browser_size,
+        e.northstar_id,
+        e.device_id
+    FROM public.snowplow_raw_events e
+    LEFT JOIN public.campaign_info i ON i.campaign_id = e.campaign_id::bigint
+);
+
+DROP INDEX IF EXISTS spe_unique;
+DROP INDEX IF EXISTS spe_session_id;
+TRUNCATE public.snowplow_phoenix_events;
+INSERT INTO public.snowplow_phoenix_events
+  SELECT * FROM public.snowplow_phoenix_events_stage;
+CREATE UNIQUE INDEX spe_unique ON public.snowplow_phoenix_events (event_datetime, event_name, event_id);
+CREATE INDEX spe_session_id ON public.snowplow_phoenix_events (session_id);
+DROP TABLE IF EXISTS public.snowplow_phoenix_events_stage;
+
+DROP TABLE IF EXISTS public.snowplow_sessions_stage;
+CREATE TABLE public.snowplow_sessions_stage AS (
+    WITH sessions AS (
+    SELECT
+        session_id,
+        min(device_id) AS device_id,
+        min(event_datetime) AS landing_datetime,
+        max(event_datetime) AS ending_datetime,
+        date_part(
+        'seconds', max(event_datetime) - min(event_datetime)
+        ) AS session_duration_seconds,
+        count(DISTINCT CASE WHEN event_name = 'view' THEN "path" END) AS num_pages_viewed
+    FROM snowplow_phoenix_events
+    GROUP BY session_id
+    ),
+    entry_exit_pages AS (
+    SELECT DISTINCT
+        session_id,
+        first_value("path") OVER (PARTITION BY session_id ORDER BY event_datetime 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS landing_page,
+        last_value("path") OVER (PARTITION BY session_id ORDER BY event_datetime 
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS exit_page
+    FROM snowplow_phoenix_events
+    ),
+    time_between_sessions AS (
+    SELECT DISTINCT
+        device_id,
+        session_id,
+        LAG(ending_datetime) OVER (PARTITION BY device_id ORDER BY landing_datetime
+        ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
+        ) AS prev_session_endtime
+    FROM sessions
+    )
+    SELECT
+    s.session_id,
+    s.device_id,
+    s.landing_datetime,
+    s.ending_datetime,
+    s.session_duration_seconds,
+    s.num_pages_viewed,
+    p.landing_page,
+    p.exit_page,
+    date_part('day', s.landing_datetime - t.prev_session_endtime) AS days_since_last_session
+    FROM sessions s
+    LEFT JOIN entry_exit_pages p
+    ON p.session_id = s.session_id
+    LEFT JOIN time_between_sessions t
+    ON t.session_id = s.session_id
+);
+
+DROP INDEX IF EXISTS sps_landing;
+TRUNCATE public.snowplow_sessions;
+INSERT INTO public.snowplow_sessions
+  SELECT * FROM public.snowplow_sessions_stage;
+CREATE INDEX sps_landing ON public.snowplow_sessions (landing_datetime, landing_page);
+DROP TABLE IF EXISTS public.snowplow_sessions_stage;
