0963974748268d243c514f18bd6258f852fa9559
Morgan Rich <mrich@dosomething.org>
2019-07-10 14:29:43 -0400

    Adding utm params to users source details

--- data/sql/derived-tables/users_table.sql
+++ data/sql/derived-tables/users_table.sql
@@ -54,6 +54,9 @@ CREATE MATERIALIZED VIEW public.users AS
 		email_status."timestamp" AS cio_status_timestamp,
 		u.sms_status,
 		u.source_detail,
+		substring(source_detail from '(?<=utm_medium\:)(.*)(?=\,)') AS utm_medium,
+		substring(source_detail from '(?<=utm_source\:)([^\,]*)') AS utm_source,
+		substring(source_detail from '(?<=utm_campaign\:)(.*)(?=)') AS utm_campaign
 		CASE WHEN 
 			u.sms_status in ('active','less','pending') OR 
 			email_status.event_type = 'customer_subscribed' 
