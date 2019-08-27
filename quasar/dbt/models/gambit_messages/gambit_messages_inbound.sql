24c233b2d6507838b372cf15f535f06e7d5778ab
Sena Heydari <sheyd@users.noreply.github.com>
2019-08-27 14:36:01 -0400

    Adding gambit tables back. (#803)
    * Adding gambit tables back.
    
    * Adding newlines to SQL files.

---
+++ quasar/dbt/models/gambit_messages/gambit_messages_inbound.sql
@@ -0,0 +1,41 @@
+SELECT
+  *
+  FROM
+    {{ ref('messages_flattened') }} f
+  WHERE
+    f.direction = 'inbound'
+    AND f.user_id IS NOT NULL
+  UNION ALL
+    (SELECT
+      g.agent_id,
+      g.attachment_url,
+      g.attachment_content_type,
+      g.broadcast_id,
+      g.campaign_id,
+      g.conversation_id,
+      g.created_at,
+      g.direction,
+      g.message_id,
+      g.macro,
+      g."match",
+      g.carrier_delivered_at,
+      g.carrier_failure_code,
+      g.total_segments,
+      g.platform_message_id,
+      g.template,
+      g.text,
+      g.topic,
+      u.northstar_id AS user_id
+    FROM
+      {{ ref('messages_flattened') }} g
+    LEFT JOIN
+      {{ env_var('FT_GAMBIT_CONVERSATIONS') }} c
+    ON g.conversation_id = c._id
+    LEFT JOIN
+      public.users u
+      ON substring(c.platform_user_id, 3, 10) = u.mobile
+      AND u.mobile IS NOT NULL
+      AND u.mobile <> ''
+    WHERE
+      g.direction = 'inbound'
+      AND g.user_id IS NULL)
---
+++ quasar/dbt/models/gambit_messages/gambit_messages_outbound.sql
@@ -0,0 +1,51 @@
+SELECT
+    f.campaign_id,
+    f.conversation_id,
+    f.broadcast_id,
+    f.created_at,
+    f.direction,
+    f.message_id,
+    f.macro,
+    f."match",
+    f.carrier_delivered_at,
+    f.carrier_failure_code,
+    f.platform_message_id,
+    f.template,
+    f.text,
+    f.topic,
+    f.user_id
+  FROM
+    {{ ref('messages_flattened') }} f
+  WHERE
+    f.direction <> 'inbound'
+    AND f.user_id IS NOT NULL
+  UNION ALL
+    (SELECT
+      g.campaign_id,
+      g.conversation_id,
+      g.broadcast_id,
+      g.created_at,
+      g.direction,
+      g.message_id,
+      g.macro,
+      g."match",
+      g.carrier_delivered_at,
+      g.carrier_failure_code,
+      g.platform_message_id,
+      g.template,
+      g.text,
+      g.topic,
+      u.northstar_id AS user_id
+      FROM
+        {{ ref('messages_flattened') }} g
+      LEFT JOIN
+        {{ env_var('FT_GAMBIT_CONVERSATIONS') }} c
+      ON g.conversation_id = c._id
+      LEFT JOIN
+        public.users u
+      ON substring(c.platform_user_id, 3, 10) = u.mobile
+        AND u.mobile IS NOT NULL
+        AND u.mobile <> ''
+      WHERE
+        g.direction <> 'inbound'
+        AND g.user_id IS NULL)
