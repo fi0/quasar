ade694572747938b5c8fefb41bc0af8202ccf9d5
Morgan Rich <mrich@dosomething.org>
2019-09-20 16:59:07 -0400

    Updating logic to use audit updated_at data

--- quasar/dbt/news_subscription/email_subscription_topics.sql
+++ quasar/dbt/news_subscription/email_subscription_topics.sql
@@ -1,6 +1,6 @@
-SELECT 
+SELECT DISTINCT
 	_id as id,
-	updated_at,
+	(audit #>> '{email_subscription_topics,updated_at,date}')::timestamptz AS email_subs_updated,
 	CASE 
 		WHEN u.email_subscription_topics::text like '%community%'
 		THEN true 
@@ -22,3 +22,4 @@ SELECT
 		ELSE NULL END 
 		AS "scholarships_subscription_status"
 FROM {{ env_var('NORTHSTAR_FIVETRAN') }}.northstar_users_snapshot u
+WHERE audit #>> '{email_subscription_topics,updated_at,date}' IS NOT null
