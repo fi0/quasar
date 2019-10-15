89da8c34c4af45c3efa2d93c6c41625f0e09223c
Morgan Rich <mrich@dosomething.org>
2019-10-15 12:33:49 -0700

    Updating newsletter tables to be aware of email unsubs (#835)
    * Updating newsletter tables to be aware of email unsubs
    
    * Updating timestamp for topic_updated_at

--- quasar/dbt/models/news_subscription/email_subscription_topics_raw.sql
+++ quasar/dbt/models/news_subscription/email_subscription_topics_raw.sql
@@ -3,4 +3,4 @@ SELECT DISTINCT
 	(audit #>> '{email_subscription_topics,updated_at,date}')::timestamp AS newsletter_updated_at,
 	json_array_elements(u.email_subscription_topics)::TEXT AS newsletter_topic
 FROM {{ env_var("NORTHSTAR_FT_SCHEMA") }}.northstar_users_snapshot u
-WHERE audit #>> '{email_subscription_topics,updated_at,date}' IS NOT null
+WHERE audit #>> '{email_subscription_topics,updated_at,date}' IS NOT NULL
--- quasar/dbt/models/news_subscription/user_newsletter_subscriptions.sql
+++ quasar/dbt/models/news_subscription/user_newsletter_subscriptions.sql
@@ -1,11 +1,13 @@
 SELECT DISTINCT
 	f.northstar_id,
 	f.newsletter_topic,
-	f.topic_subscribed_at,
-	CASE WHEN topic_unsubscribed_at IS NOT NULL 
+	f.topic_subscribed_at::timestamp,
+	CASE WHEN newsletters_unsubscribed_at IS NOT NULL
+	    THEN newsletters_unsubscribed_at
+	    WHEN topic_unsubscribed_at IS NOT NULL
 		THEN topic_unsubscribed_at
 		WHEN f.topic_updated_at = f.user_updated_at
-		THEN NULL 
+		THEN NULL
 		ELSE f.user_updated_at END AS topic_unsubscribed_at
 FROM (
 	SELECT DISTINCT
@@ -17,14 +19,22 @@ FROM (
 			ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS topic_updated_at,	
 		last_value(s.newsletter_updated_at) OVER (PARTITION BY s.northstar_id ORDER BY s.newsletter_updated_at
 			ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS user_updated_at,
-		u.topic_unsubscribed_at
+		u.topic_unsubscribed_at,
+		ua.newsletters_unsubscribed_at
 	FROM {{ ref('email_subscription_topics_raw') }} s
 	LEFT JOIN (
-		SELECT 
+		SELECT
 			_id AS id,
 			to_timestamp(audit #>> '{email_subscription_topics,updated_at,date}', 'YYYY-MM-DD HH24:MI:SS') AS topic_unsubscribed_at,
 			NULL AS newsletter_topic
 		FROM {{ env_var("NORTHSTAR_FT_SCHEMA") }}.northstar_users_snapshot
-		WHERE email_subscription_topics IS NULL 
+		WHERE email_subscription_topics IS NULL AND audit #>> '{email_subscription_topics,updated_at,date}' IS NOT NULL
 	) u ON u.id = s.northstar_id
+    LEFT JOIN (
+		SELECT
+			northstar_id,
+			email_unsubscribed_at AS newsletters_unsubscribed_at,
+			NULL AS newsletter_topic
+		FROM public.user_activity
+	) ua ON ua.northstar_id = s.northstar_id
 ) f
