80647850bc362bd039d66fefa9f07f5a7421ab2b
Rafa <rapala61@gmail.com>
2019-11-01 18:10:30 -0500

    Fixes path in mel:create_for_dbt_validation

--- quasar/mel.py
+++ quasar/mel.py
@@ -5,7 +5,7 @@ def create():
     run_sql_file_raw('./data/sql/derived-tables/mel.sql')
 
 def create_for_dbt_validation():
-    run_sql_file_raw('../data/sql/derived-tables/mel_dbt_validation.sql')
+    run_sql_file_raw('./data/sql/derived-tables/mel_dbt_validation.sql')
 
 def refresh():
     refresh_materialized_view('public.member_event_log')
