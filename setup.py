386a532bdb998604a7701547d1a851f7d917cadc
Sena Heydari <sheyd@users.noreply.github.com>
2019-11-05 15:11:04 -0500

    Adding IF NOT EXISTS index statements to campaign-activity. (#863)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = convert_deps_to_pip(pfile['packages'], r=False)
 
 setup(
     name="quasar",
-    version="2019.11.05.0",
+    version="2019.11.05.1",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
