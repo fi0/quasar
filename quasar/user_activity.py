34e22bb45df6390598f056b823da382760c79470
Rafa <rapala61@gmail.com>
2019-11-15 12:59:48 -0500

    remove dbt validation derived tables and jobs.

--- quasar/user_activity.py
+++ quasar/user_activity.py
@@ -5,11 +5,6 @@ def create():
     run_sql_file_raw('./data/sql/derived-tables/user_activity.sql')
 
 
-def create_for_dbt_validation():
-    run_sql_file_raw(''.join(("./data/sql/derived-tables/"
-                              "user_activity_dbt_validation.sql")))
-
-
 def refresh():
     refresh_materialized_view('public.user_activity')
 
