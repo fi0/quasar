4148af8e02b99063bf623dcd26b0a6a154da74aa
Rafa <rapala61@gmail.com>
2020-01-06 15:55:39 -0500

    Fixes wrong keys in turbovote model

--- quasar/dbt/models/campaign_activity/turbovote.sql
+++ quasar/dbt/models/campaign_activity/turbovote.sql
@@ -1,15 +1,15 @@
 SELECT id AS post_id, 
 	details::jsonb->>'hostname' AS hostname,
-	details::jsonb->>'referral_code' AS referral_code,
-	details::jsonb->>'partner_comms_opt_in' AS partner_comms_opt_in,
+	details::jsonb->>'referral-code' AS referral_code,
+	details::jsonb->>'partner-comms-opt-in' AS partner_comms_opt_in,
 	(details::jsonb->>'created-at')::timestamp AS created_at,
 	(details::jsonb->>'updated-at')::timestamp AS updated_at,
 	source_details,
-	details::jsonb->>'voter_registration_status' AS voter_registration_status,
-	details::jsonb->>'voter_registration_source' AS voter_registration_source,
-	details::jsonb->>'voter_registration_method' AS voter_registration_method,
-	details::jsonb->>'voter_registration_preference' AS voter_registration_preference,
-	details::jsonb->>'email_subscribed' AS email_subscribed,
-	details::jsonb->>'sms_subscribed' AS sms_subscribed
+	details::jsonb->>'voter-registration-status' AS voter_registration_status,
+	details::jsonb->>'voter-registration-source' AS voter_registration_source,
+	details::jsonb->>'voter-registration-method' AS voter_registration_method,
+	details::jsonb->>'voter-registration-preference' AS voter_registration_preference,
+	details::jsonb->>'email subscribed' AS email_subscribed,
+	details::jsonb->>'sms subscribed' AS sms_subscribed
 FROM {{ env_var('FT_ROGUE') }}.posts
 WHERE source = 'turbovote'
