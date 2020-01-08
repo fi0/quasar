5ee801c48ad534528739675e9e8e9992b66448e9
Sena Heydari <sheyd@users.noreply.github.com>
2020-01-08 11:27:10 -0500

    Voter reg method syntax fix (#923)
    * Fixing single json object key name for turbovote table.
    
    * Version update.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = convert_deps_to_pip(pfile['packages'], r=False)
 
 setup(
     name="quasar",
-    version="2020.01.03.0",
+    version="2020.01.07.0",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
