001714b689d377eeded910cc5be77414b676ab12
Rafa <rapala61@gmail.com>
2020-01-10 13:02:13 -0500

    Regenerates docs to remove campaign_info_all

--- docs/compiled/ds_dbt/campaign_activity/turbovote.sql
+++ docs/compiled/ds_dbt/campaign_activity/turbovote.sql
@@ -8,7 +8,7 @@ SELECT id AS post_id,
 	details::jsonb->>'voter-registration-status' AS voter_registration_status,
 	details::jsonb->>'voter-registration-source' AS voter_registration_source,
 	details::jsonb->>'voter-registration-method' AS voter_registration_method,
-	details::jsonb->>'voter-registration-preference' AS voter_registration_preference,
+	details::jsonb->>'voting-method-preference' AS voter_registration_preference,
 	details::jsonb->>'email subscribed' AS email_subscribed,
 	details::jsonb->>'sms subscribed' AS sms_subscribed
 FROM ft_dosomething_rogue.posts
--- docs/compiled/ds_dbt/campaign_info/campaign_info_international.sql
+++ docs/compiled/ds_dbt/campaign_info/campaign_info_international.sql
@@ -2,6 +2,6 @@ SELECT
 	c.id AS campaign_id,
 	c.internal_title AS campaign_name,
 	i.*
-FROM "quasar_prod_warehouse"."public"."campaign_info_all" i
+FROM public.campaign_info_ashes_snapshot i
 LEFT JOIN ft_dosomething_rogue.campaigns c ON i.campaign_run_id = c.campaign_run_id
 WHERE campaign_language IS DISTINCT FROM 'en'
