223e3716abcf603d969ea07765a867dd7a01e97c
Rafa <rapala61@gmail.com>
2020-02-10 17:28:37 -0500

    Regenerated docs

--- docs/compiled/ds_dbt/ft_northstar_migration_testing/user_activity_fnmt.sql
+++ docs/compiled/ds_dbt/ft_northstar_migration_testing/user_activity_fnmt.sql
@@ -36,7 +36,7 @@ gambit_unsub AS (
 sms_undeliverable AS (
     SELECT DISTINCT
 	    northstar_id,
-	    FIRST_VALUE(updated_at) OVER (PARTITION BY id ORDER BY updated_at
+	    FIRST_VALUE(updated_at) OVER (PARTITION BY northstar_id ORDER BY updated_at
 		ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) AS unsub_ts
     FROM "quasar_prod_warehouse"."public"."northstar_users_deduped_fnmt"
     WHERE sms_status IN ('unknown', 'undeliverable', 'GDPR')
@@ -147,7 +147,7 @@ ON u.northstar_id = email_opens.customer_id
 LEFT JOIN gambit_unsub
 ON u.northstar_id = gambit_unsub.user_id
 LEFT JOIN sms_undeliverable
-ON u.northstar_id = sms_undeliverable.id
+ON u.northstar_id = sms_undeliverable.northstar_id
 LEFT JOIN email_unsub
 ON u.northstar_id = email_unsub.customer_id
 LEFT JOIN time_to_actions
