737bb364b9fc4f71738c11e2bdb4a33b00ff0776
Sena Heydari <sheyd@users.noreply.github.com>
2020-03-18 15:30:04 -0400

    Adding additional TMC permissions needed for Looker. (#978)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = convert_deps_to_pip(pfile['packages'], r=False)
 
 setup(
     name="quasar",
-    version="2020.3.16.0",
+    version="2020.3.18.0",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
