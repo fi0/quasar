4c06ddc1d1cc0d027d1255e38268d0b96436b83d
Morgan Rich <mrich@dosomething.org>
2020-04-02 09:49:14 -0400

    Updating OVRD recipient funnel

--- quasar/dbt/models/user_journey/ovrd_recipient_funnel.sql
+++ quasar/dbt/models/user_journey/ovrd_recipient_funnel.sql
@@ -1,6 +1,10 @@
 SELECT 
-	po.northstar_id,
-	min(rtv.started_registration) AS journey_begin_ts,
+	bc.northstar_id,
+	min(bc.click_time) AS journey_begin_ts,
+	max(
+		CASE
+			WHEN bc.click_id IS NOT NULL THEN 1 ELSE 0 END
+		) AS clicked_link,
 	max(
 		CASE 
 			WHEN rtv.status IS NOT NULL THEN 1 ELSE 0 END
@@ -9,10 +13,14 @@ SELECT
 		CASE 
 			WHEN rb.northstar_id IS NOT NULL THEN 1 ELSE 0 END 
 	) AS completed_registration
-FROM {{ ref('rock_the_vote') }} rtv 
-LEFT JOIN {{ ref('posts') }} po ON rtv.post_id=po.id
+FROM {{ ref('bertly_clicks') }} bc
+LEFT JOIN {{ ref('posts') }} po ON bc.northstar_id=po.northstar_id
+LEFT JOIN {{ ref('rock_the_vote') }} rtv ON po.id=rtv.post_id
 LEFT JOIN {{ ref('reportbacks') }} rb ON rtv.post_id=rb.post_id AND rb.post_bucket = 'voter_registrations'
 WHERE 
-	rtv.tracking_source ILIKE '%referral=true%'
-	AND rtv.started_registration >= '2018-01-01'
-GROUP BY po.northstar_id
+	bc.target_url ILIKE '%vote.dosomething.org/member-drive%'
+	AND bc.northstar_id IS NOT NULL
+	AND bc.target_url ILIKE '%referral=true%'
+	AND bc.interaction_type = 'click'
+	AND (rtv.started_registration >= '2018-01-01' OR rtv.started_registration IS NULL)
+GROUP BY bc.northstar_id
