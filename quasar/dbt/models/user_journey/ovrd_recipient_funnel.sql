63a6acd4eff3462a3809680e44b543c6bd28a4f8
Sena Heydari <sheyd@users.noreply.github.com>
2020-04-10 12:28:22 -0400

    Updating missing FROM reference in OVRD funnel. (#997)
    * Updating missing FROM reference in OVRD funnel.
    
    * Removing the bertly clicks references again.

--- quasar/dbt/models/user_journey/ovrd_recipient_funnel.sql
+++ quasar/dbt/models/user_journey/ovrd_recipient_funnel.sql
@@ -1,5 +1,5 @@
 SELECT 
-	bc.northstar_id,
+	po.northstar_id,
 	min(rtv.started_registration) AS journey_begin_ts,
 	max(
 		CASE 
@@ -9,10 +9,10 @@ SELECT
 		CASE 
 			WHEN rb.northstar_id IS NOT NULL THEN 1 ELSE 0 END 
 	) AS completed_registration
-FROM {{ ref('posts') }} po ON bc.northstar_id=po.northstar_id
-LEFT JOIN {{ ref('rock_the_vote') }} rtv ON po.id=rtv.post_id
-LEFT JOIN {{ ref('reportbacks') }} rb ON rtv.post_id=rb.post_id AND rb.post_bucket = 'voter_registrations'
+FROM {{ ref('posts') }} po
+LEFT JOIN {{ ref('rock_the_vote') }} rtv ON po.id = rtv.post_id
+LEFT JOIN {{ ref('reportbacks') }} rb ON rtv.post_id = rb.post_id AND rb.post_bucket = 'voter_registrations'
 WHERE 
 	rtv.tracking_source ILIKE '%referral=true%'
 	AND (rtv.started_registration >= '2018-01-01' OR rtv.started_registration IS NULL)
-GROUP BY bc.northstar_id
+GROUP BY po.northstar_id
