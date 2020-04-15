360fd2cd620ffb3094eaa634a46bccdd73d11dca
Sena Heydari <sheyd@users.noreply.github.com>
2020-04-15 10:29:32 -0400

    Updating Customer.io SQL updates to raw tables. (#1001)

--- quasar/cio_import_scratch_records.py
+++ quasar/cio_import_scratch_records.py
@@ -5,26 +5,11 @@ db = Database()
 duration = Duration()
 
 
-def import_records_event(table):
-    # Import records from cio staging tables that are populated
-    # by the cio consumer into primary queried tables that have
-    # event_type based primary key.
-    scratch = table + '_scratch'
-    query = ''.join(("INSERT INTO {} SELECT * FROM {} "
-                     "ON CONFLICT (email_id, customer_id, timestamp, "
-                     "event_type) DO NOTHING"
-                     "")).format(table, scratch)
-    db.query(query)
-
-
 def import_records(table):
     # Import records from cio staging tables that are populated
     # by the cio consumer into primary queried tables.
     scratch = table + '_scratch'
-    query = ''.join(("INSERT INTO {} SELECT * FROM {} "
-                     "ON CONFLICT (email_id, customer_id, timestamp) "
-                     "DO NOTHING"
-                     "")).format(table, scratch)
+    query = ('INSERT INTO {} SELECT * FROM {}').format(table, scratch)
     db.query(query)
 
 
@@ -38,17 +23,13 @@ def truncate_scratch(table):
 
 def cio_import():
     # List of cio tables to process.
-    tables = ['cio.email_sent', 'cio.email_bounced']
-    event_tables = ['cio.customer_event', 'cio.email_event']
+    tables = ['cio.email_sent',
+              'cio.email_bounced',
+              'cio.customer_event',
+              'cio.email_event']
     for table in tables:
         log("Importing records for table {}.".format(table))
         import_records(table)
-        scratch = table + '_scratch'
-        log("Truncating table {}.".format(scratch))
-        truncate_scratch(table)
-    for table in event_tables:
-        log("Importing records for table {}.".format(table))
-        import_records_event(table)
         scratch = table + '_scratch'
         log("Truncating table {}.".format(scratch))
         truncate_scratch(table)
