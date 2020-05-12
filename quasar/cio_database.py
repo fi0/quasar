c39588c2c70e40942b3571f3f7fa73705f752d1b
Sena Heydari <sheydari@dosomething.org>
2020-05-11 21:27:48 -0400

    Added rest of data backend and insert/commit records and full event log per insert.

--- quasar/cio_database.py
+++ quasar/cio_database.py
@@ -48,54 +48,79 @@ class Database:
             logerr(e)
 
     def _create_customer_event_table(self):
-            self.customer_event = Table('customer_event_scratch', self.meta,
-                Column('email_id', String),
-                Column('customer_id', String),
-                Column('email_address', String),
-                Column('template_id', Integer),
-                Column('event_id', String),
-                Column('timestamp', DateTime(timezone=True)),
-                Column('event_type', String),
-                schema='cio')
+        self.customer_event = Table('customer_event_scratch', self.meta,
+            Column('email_id', String),
+            Column('customer_id', String),
+            Column('email_address', String),
+            Column('template_id', Integer),
+            Column('event_id', String),
+            Column('timestamp', DateTime(timezone=True)),
+            Column('event_type', String),
+            schema='cio')
 
     def _create_email_event_table(self):
-            self.email_event = Table('email_event_scratch', self.meta,
-                Column('email_id', String),
-                Column('customer_id', String),
-                Column('email_address', String),
-                Column('template_id', Integer),
-                Column('subject', Text),
-                Column('href', Text),
-                Column('link_id', String),
-                Column('event_id', String),
-                Column('timestamp', DateTime(timezone=True)),
-                Column('event_type', String),
-                schema='cio')
+        self.email_event = Table('email_event_scratch', self.meta,
+            Column('email_id', String),
+            Column('customer_id', String),
+            Column('email_address', String),
+            Column('template_id', Integer),
+            Column('subject', Text),
+            Column('href', Text),
+            Column('link_id', String),
+            Column('event_id', String),
+            Column('timestamp', DateTime(timezone=True)),
+            Column('event_type', String),
+            schema='cio')
 
     def _create_email_bounced_table(self):
-            self.email_bounced = Table('email_bounced_scratch', self.meta,
-                Column('email_id', String),
-                Column('customer_id', String),
-                Column('email_address', String),
-                Column('template_id', Integer),
-                Column('event_id', String),
-                Column('timestamp', DateTime(timezone=True)),
-                schema='cio')
+        self.email_bounced = Table('email_bounced_scratch', self.meta,
+            Column('email_id', String),
+            Column('customer_id', String),
+            Column('email_address', String),
+            Column('template_id', Integer),
+            Column('event_id', String),
+            Column('timestamp', DateTime(timezone=True)),
+            schema='cio')
 
     def _create_email_sent_table(self):
-            self.email_sent = Table('email_sent_scratch', self.meta,
-                Column('email_id', String),
-                Column('customer_id', String),
-                Column('email_address', String),
-                Column('template_id', Integer),
-                Column('event_id', String),
-                Column('timestamp', DateTime(timezone=True)),
-                schema='cio')
+        self.email_sent = Table('email_sent_scratch', self.meta,
+            Column('email_id', String),
+            Column('customer_id', String),
+            Column('email_address', String),
+            Column('template_id', Integer),
+            Column('event_id', String),
+            Column('timestamp', DateTime(timezone=True)),
+            schema='cio')
 
     def _create_event_log_table(self):
-            self.event_log = Table('event_log', self.meta,
-                Column('event', JSONB),
-                schema='cio')
+        self.event_log = Table('event_log', self.meta,
+            Column('event', JSONB),
+            schema='cio')
+
+    def _commit_event(data):
+        # Copy data to event_log table and commit to database.
+        self.event_log.insert().values(data)
+        self.conn.execute()
+
+    def insert_customer(data):
+        # Pass in dictionary to insert sub/unsub event.
+        self.customer_event.insert().values(data)
+        self._commit_event(data)
+
+    def insert_email(data):
+        # Pass in dictionary to insert email open/click, etc. event.
+        self.email_event.insert().values(data)
+        self._commit_event(data)
+
+    def insert_email_bounced(data):
+        # Pass in dictionary to insert email bounce event.
+        self.email_bounced.insert().values(data)
+        self._commit_event(data)
+
+    def insert_email_sent(data):
+        # Pass in dictionary to insert email sent event.
+        self.email_sent.insert().values(data)
+        self._commit_event(data)
 
     def disconnect(self):
         self.conn.close()
