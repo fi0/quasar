52c295b41cb21b254dd872d69d8c944f4f82150e
Morgan Rich <mrich@dosomething.org>
2020-05-14 14:18:42 -0400

    Adding code to pull data from Contentful API

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = convert_deps_to_pip(pfile['packages'], r=False)
 
 setup(
     name="quasar",
-    version="2020.5.4.2",
+    version="2020.5.14.2",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
