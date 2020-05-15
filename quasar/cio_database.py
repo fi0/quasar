1ee68ce883b0e75febf39767d582910d6fc805cb
Sena Heydari <sheydari@dosomething.org>
2020-05-14 23:28:39 -0400

    Updating code to use SQL Alchemy and simplify logic to ingest entire JSON message and timestamp.

--- quasar/cio_database.py
+++ quasar/cio_database.py
@@ -34,11 +34,7 @@ class Database:
                             connect_args={'sslmode': pg_ssl})
             self.conn = self.engine.connect()
             self.meta = MetaData()
-            # Define each Customer.io table with 1 table/private function.
-            self._create_customer_event_table()
-            self._create_email_event_table()
-            self._create_email_bounced_table()
-            self._create_email_sent_table()
+            # Define event_log table.
             self._create_event_log_table()
             # Once all tables are defined, initialize them.
             self.meta.create_all(self.engine)
@@ -47,89 +43,17 @@ class Database:
             log("Error is:")
             logerr(e)
 
-    def _create_customer_event_table(self):
-        self.customer_event = Table('customer_event_scratch', self.meta,
-            Column('email_id', String),
-            Column('customer_id', String),
-            Column('email_address', String),
-            Column('template_id', Integer),
-            Column('event_id', String),
-            Column('timestamp', DateTime(timezone=True)),
-            Column('event_type', String),
-            schema='cio')
-        return self.customer_event
-
-    def _create_email_event_table(self):
-        self.email_event = Table('email_event_scratch', self.meta,
-            Column('email_id', String),
-            Column('customer_id', String),
-            Column('email_address', String),
-            Column('template_id', Integer),
-            Column('subject', Text),
-            Column('href', Text),
-            Column('link_id', String),
-            Column('event_id', String),
-            Column('timestamp', DateTime(timezone=True)),
-            Column('event_type', String),
-            schema='cio')
-        return self.email_event
-
-    def _create_email_bounced_table(self):
-        self.email_bounced = Table('email_bounced_scratch', self.meta,
-            Column('email_id', String),
-            Column('customer_id', String),
-            Column('email_address', String),
-            Column('template_id', Integer),
-            Column('event_id', String),
-            Column('timestamp', DateTime(timezone=True)),
-            schema='cio')
-        return self.email_bounced
-
-    def _create_email_sent_table(self):
-        self.email_sent = Table('email_sent_scratch', self.meta,
-            Column('email_id', String),
-            Column('customer_id', String),
-            Column('email_address', String),
-            Column('template_id', Integer),
-            Column('event_id', String),
-            Column('timestamp', DateTime(timezone=True)),
-            schema='cio')
-        return self.email_sent
-
     def _create_event_log_table(self):
         self.event_log = Table('event_log', self.meta,
             Column('event', JSONB),
+            Column('timestamp', DateTime(timezone=True)),
             schema='cio')
         return self.event_log
 
-    def commit_event(self, data, query=None):
-        # Copy data to event_log table and commit to database.
-        log_query = self.event_log.insert().values(data)
-        self.conn.execute(log_query)
-        if query is not None:
-            self.conn.execute(query)
-        else:
-            pass
-
-    def insert_customer(self, data):
-        # Pass in dictionary to insert sub/unsub event.
-        query = self.customer_event.insert().values(data)
-        self.commit_event(data, query)
-
-    def insert_email(self, data):
-        # Pass in dictionary to insert email open/click, etc. event.
-        query = self.email_event.insert().values(data)
-        self.commit_event(data, query)
-
-    def insert_email_bounced(self, data):
-        # Pass in dictionary to insert email bounce event.
-        query = self.email_bounced.insert().values(data)
-        self.commit_event(data, query)
-
-    def insert_email_sent(self, data):
-        # Pass in dictionary to insert email sent event.
-        query = self.email_sent.insert().values(data)
-        self.commit_event(data, query)
+    def insert_event(self, data, timestamp):
+        # Insert event JSON and timestamp values.
+        query = self.event_log.insert().values(event=data, timestamp=timestamp)
+        self.conn.execute(query)
 
     def disconnect(self):
         self.conn.close()
--- quasar/cio_queue.py
+++ quasar/cio_queue.py
@@ -18,66 +18,14 @@ class CioQueue(QuasarQueue):
                          self.blink_exchange)
         self.db = Database()
 
-    # Save customer sub data and dates.
-    def _add_customer_event(self, data):
-        self.db.insert_customer(data)
-        log(''.join(("Added customer event from "
-                     "C.IO event id {}.")).format(data['event_id']))
-
-    # Save email event data and dates, e.g. email_click.
-    def _add_email_event(self, data):
-        self.db.insert_email(data)
-        log(''.join(("Added email event from "
-                     "C.IO event id {}.")).format(data['event_id']))
-
-    # Save email sent event.
-    def _add_email_sent_event(self, data):
-        self.db.insert_email_sent(data)
-        log(''.join(("Added email sent event from "
-                     "C.IO event id {}.")).format(data['event_id']))
-
-    # Save email bounced event.
-    def _add_email_bounced_event(self, data):
-        self.db.insert_email_bounced(data)
-        log(''.join(("Added email bounced event from "
-                     "C.IO event id {}.")).format(data['event_id']))
-
     def process_message(self, message_data):
+        # Extract data subfield.
         data = message_data['data']
-        event_type = pydash.get(data, 'event_type')
-        print(data)
-        # Set for checking email event types.
-        email_event = {
-            'email_bounced',
-            'email_converted',
-            'email_opened',
-            'email_unsubscribed',
-            'email_clicked'
-        }
+        # Get timestamp entry.
+        timestamp = data['timestamp']
         try:
-            if (event_type == 'customer_subscribed' or
-                    event_type == 'customer_unsubscribed'):
-                self._add_customer_event(data)
-            elif event_type in email_event:
-                self._add_email_event(data)
-            elif event_type == 'email_sent':
-                self._add_email_sent_event(data)
-            elif event_type == 'email_bounced':
-                self._add_email_bounced_event(data)
-            else:
-                pass
-        except KeyError as e:
-            logerr("C.IO message missing {}".format(e))
-            # Actually log missing data and exit so not dropping data.
-            logerr("Message data is:")
-            logerr(format(message_data))
-            # Record error data to event_log and disconnect cleanly.
-            self.db.commit_event(data)
-            self.db.disconnect()
-            sys.exit(1)
+            self.db.insert_event(data, timestamp)
         except:
             logerr("Something went wrong with C.IO consumer!")
-            # Record error data to event_log and disconnect cleanly.
-            self.db.commit_event(data)
             self.db.disconnect()
             sys.exit(1)
