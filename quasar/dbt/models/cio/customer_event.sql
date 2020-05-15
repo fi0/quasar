2861e3b5be64fdd51702ae3bbbb1ebceeff13d26
Rafa <rapala61@gmail.com>
2020-05-15 15:01:02 -0500

    Adds the actual changes to customer_event

--- quasar/dbt/models/cio/customer_event.sql
+++ quasar/dbt/models/cio/customer_event.sql
@@ -1,10 +1,18 @@
 SELECT
-    event #>>'{data, email_id}' as email_id,
+SELECT
+    COALESCE(
+        event #>>'{data, email_id}',
+        event #>>'{data, variables, email_id}'
+    ) AS email_id,
     event #>>'{data, customer_id}' as customer_id,
     event #>>'{data, email_address}' as email_address,
+    event #>>'{data, template_id}' as template_id,
     event ->> 'event_id' AS event_id,
     TO_TIMESTAMP(CAST(event ->> 'timestamp' AS INTEGER)) AS "timestamp",
-    event ->> 'event_type' AS event_type
+    event ->> 'event_type' AS event_type,
+    event #>>'{data, variables, campaign, id}' as cio_campaign_id,
+    event #>>'{data, variables, campaign, name}' as cio_campaign_name,
+    event #>>'{data, variables, campaign, type}' as cio_campaign_type
 FROM
     { { source('cio', 'event_log') } } cel
 WHERE
