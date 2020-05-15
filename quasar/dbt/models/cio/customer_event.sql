ae26cbbaf9a83835538504e387bd1fd4fb440504
Rafa <rapala61@gmail.com>
2020-05-15 14:47:50 -0500

    Implements SQL for customer_event

--- quasar/dbt/models/cio/customer_event.sql
+++ quasar/dbt/models/cio/customer_event.sql
@@ -1,3 +1,11 @@
 SELECT
-*
-FROM {{ source('cio', 'event_log') }} cel
+    event #>>'{data, email_id}' as email_id,
+    event #>>'{data, customer_id}' as customer_id,
+    event #>>'{data, email_address}' as email_address,
+    event ->> 'event_id' AS event_id,
+    TO_TIMESTAMP(cast(event ->> 'timestamp' AS INTEGER)) AS "timestamp",
+    event ->> 'event_type' AS event_type
+FROM
+    { { source('cio', 'event_log') } } cel
+WHERE
+    event ->> 'event_type' IN ('customer_subscribed', 'customer_unsubscribed')
