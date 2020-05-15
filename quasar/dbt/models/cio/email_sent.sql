a8c9159c84159deb284924339b68005cc77d7b66
Rafa <rapala61@gmail.com>
2020-05-15 15:12:18 -0500

    Adds SQL for email_sent

--- quasar/dbt/models/cio/email_sent.sql
+++ quasar/dbt/models/cio/email_sent.sql
@@ -1,4 +1,18 @@
 SELECT
-    *
+    coalesce(
+        event #>>'{data, email_id}',
+        event #>>'{data, variables, email_id}'
+    ) AS email_id,
+    event #>>'{data, customer_id}' as customer_id,
+    event #>>'{data, email_address}' as email_address,
+    event #>>'{data, template_id}' as template_id,
+    event #>>'{data, subject}' as subject,
+    event ->> 'event_id' AS event_id,
+    TO_TIMESTAMP(CAST(event ->> 'timestamp' AS INTEGER)) AS "timestamp",
+    event #>>'{data, variables, campaign, id}' as cio_campaign_id,
+    event #>>'{data, variables, campaign, name}' as cio_campaign_name,
+    event #>>'{data, variables, campaign, type}' as cio_campaign_type
 FROM
     { { source('cio', 'event_log') } } cel
+WHERE
+    event ->> 'event_type' = 'email_sent'
