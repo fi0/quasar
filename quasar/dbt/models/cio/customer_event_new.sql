b860d6f6314126453c5b09e2da1f987ef6ec9d55
Rafa <rapala61@gmail.com>
2020-05-18 13:51:32 -0500

    Updates model names and implements unions with old legacy cio tables for customer_event_new and email_sent_new

---
+++ quasar/dbt/models/cio/customer_event_new.sql
@@ -0,0 +1,39 @@
+SELECT
+    COALESCE(
+        event #>>'{data, email_id}',
+        event #>>'{data, variables, email_id}'
+    ) AS email_id,
+    event #>>'{data, customer_id}' AS customer_id,
+    event #>>'{data, email_address}' AS email_address,
+    CAST(
+        event #>>'{data, template_id}' AS INTEGER
+    ) AS template_id,
+    event ->> 'event_id' AS event_id,
+    TO_TIMESTAMP(CAST(
+        event ->> 'timestamp' AS INTEGER
+    )) AS "timestamp",
+    event ->> 'event_type' AS event_type,
+    event #>>'{data, variables, campaign, id}' AS cio_campaign_id,
+    event #>>'{data, variables, campaign, name}' AS cio_campaign_name,
+    event #>>'{data, variables, campaign, type}' AS cio_campaign_type
+FROM
+    {{ source('cio', 'event_log') }} cel
+WHERE
+    event ->> 'event_type' IN ('customer_subscribed', 'customer_unsubscribed')
+UNION
+SELECT
+    email_id,
+    customer_id,
+    email_address,
+    template_id,
+    event_id,
+    "timestamp",
+    event_type,
+    NULL AS cio_campaign_id,
+    NULL AS cio_campaign_name,
+    NULL AS cio_campaign_type
+FROM
+    {{ source('cio', 'customer_event_old') }} cceo
+WHERE
+    -- Date we re-started saving raw C.io events to the event_log table
+    timestamp < '2020-04-01'
