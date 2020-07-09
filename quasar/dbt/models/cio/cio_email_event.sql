a7a032e8a17741a3539cb6c2f340902b6f4910e2
Rafa <rapala61@gmail.com>
2020-07-09 13:30:39 -0500

    Exposes new Cio email event fields to help tie to DS campaigns

--- quasar/dbt/models/cio/cio_email_event.sql
+++ quasar/dbt/models/cio/cio_email_event.sql
@@ -16,7 +16,9 @@ SELECT
 	TO_TIMESTAMP(CAST(event ->> 'timestamp' AS INTEGER)) AS "timestamp",
 	event #>>'{data, variables, campaign, id}' as cio_campaign_id,
 	event #>>'{data, variables, campaign, name}' as cio_campaign_name,
-	event #>>'{data, variables, campaign, type}' as cio_campaign_type
+	event #>>'{data, variables, campaign, type}' as cio_campaign_type,
+    event #>>'{data, message_id}' as cio_message_id,
+    event #>>'{data, message_name}' as cio_message_name
 FROM
     {{ source('cio', 'event_log') }} cel
 WHERE event ->> 'event_type' IN ('email_bounced', 'email_converted', 'email_opened', 'email_unsubscribed', 'email_clicked')
@@ -34,7 +36,9 @@ SELECT
     "timestamp",
     NULL AS cio_campaign_id,
     NULL AS cio_campaign_name,
-    NULL AS cio_campaign_type
+    NULL AS cio_campaign_type,
+    NULL AS cio_message_id,
+    NULL AS cio_message_name
 FROM {{ source('cio_historical', 'cio_email_event') }}
 WHERE
     -- Date we re-started saving raw C.io events to the event_log table
