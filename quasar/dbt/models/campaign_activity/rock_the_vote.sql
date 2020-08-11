8e394478ea8c7d2f4f63f61996fa78a52bec89c1
Morgan Rich <mrich@dosomething.org>
2020-08-11 10:44:28 -0400

    Adding UTC timestamp to RTV

--- quasar/dbt/models/campaign_activity/rock_the_vote.sql
+++ quasar/dbt/models/campaign_activity/rock_the_vote.sql
@@ -1,6 +1,7 @@
 SELECT id AS post_id,
    details::jsonb->>'Tracking Source' AS tracking_source,
    (details::jsonb->>'Started registration')::timestamp AS started_registration,
+   (details::jsonb->>'Started registration')::timestamptz AS started_registration_utc,
    details::jsonb->>'Finish with State' AS finish_with_state,
    details::jsonb->>'Status' AS status,
    COALESCE(details::jsonb->>'Email address',details::jsonb->>'email') AS email,
