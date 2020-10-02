5a8c9aa60deb6af5c48427c2e8ee92e5dbe50d0c
Rafa <rapala61@gmail.com>
2020-10-02 15:51:28 -0500

    Removes tests from snowplow_phoenix_events. Comments FIXME to condense into snowplow_raw_events

--- quasar/dbt/models/phoenix_events/snowplow_phoenix_events.sql
+++ quasar/dbt/models/phoenix_events/snowplow_phoenix_events.sql
@@ -1,33 +1,38 @@
+
+-- We are replicating all events from snowplow_raw_events just to add "campaign_name"
+-- FIXME: Condense with snowplow_raw_events
 SELECT DISTINCT ON (e.EVENT_ID)
-    e.event_id,
-    e.event_datetime,
-    CASE WHEN e.event_name IS NULL
-    AND e.event_type = 'pv'
-    THEN 'view' ELSE e.event_name END AS event_name,
-    e.event_source,
-    e."path",
-    e."host",
-    e.query_parameters,
+    e.block_id,
+    e.browser_size,
+    e.campaign_id,
+    i.campaign_name,
     e.clicked_link_url,
-    e.utm_source AS page_utm_source,
-    e.utm_medium AS page_utm_medium,
+    e.context_source,
+    e.context_value,
+    e.device_id,
+    e.event_datetime,
+    e.event_id,
+    CASE
+        WHEN e.event_name IS NULL
+        AND e.event_type = 'pv' THEN 'view'
+        ELSE e.event_name
+    END AS event_name,
+    e.event_source,
+    e.group_id,
+    e."host",
+    e.modal_type,
+    e.northstar_id,
+    e.page_id,
     e.utm_campaign AS page_utm_campaign,
+    e.utm_medium AS page_utm_medium,
+    e.utm_source AS page_utm_source,
+    e."path",
+    e.query_parameters,
     e.referrer_host,
     e.referrer_path,
     e.referrer_source,
-    e.campaign_id,
-    i.campaign_name,
-    e.page_id,
-    e.block_id,
-    e.group_id,
-    e.modal_type,
     e.search_query,
-    e.context_source,
-    e.context_value,
-    e.session_id,
-    e.browser_size,
-    e.northstar_id,
-    e.device_id
+    e.session_id
 FROM {{ ref('snowplow_raw_events') }} e
 LEFT JOIN {{ ref('campaign_info') }} i ON i.campaign_id = e.campaign_id::bigint
 
